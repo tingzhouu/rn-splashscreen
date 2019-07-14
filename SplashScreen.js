5cd0deac73c86eacca3d6d6cc8a8e149baea0b48
Ting Zhou <limtingzhou@gmail.com>
2019-07-14 20:50:32 +0800

    Add loading spinner to splash screen.

--- SplashScreen.js
+++ SplashScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, Image } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 class SplashSCreen extends Component {
   render() {
@@ -7,6 +8,11 @@ class SplashSCreen extends Component {
       <View
         style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}
       >
+        <Spinner
+          visible={true}
+          textContent={'Loading...'}
+          textStyle={{ color: 'white' }}
+        />
         <Image
           source={ require('./splash_screen_image.png') }
           style={{ height: 150, width: 150, resizeMode: 'contain' }}
